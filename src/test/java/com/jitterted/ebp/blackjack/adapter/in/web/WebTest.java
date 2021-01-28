2fb5cc65d1002aa9376775f521cd8dd526f6011a
Ted M. Young <tedyoung@gmail.com>
2021-01-27 21:18:09 -0800

    hitCommand redirects to /game.

--- src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/WebTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/WebTest.java
@@ -35,4 +35,11 @@ public class WebTest {
            .andExpect(status().isOk());
   }
 
+  @Test
+  public void postToHitEndpointIs3xxRedirect() throws Exception {
+    mockMvc.perform(post("/hit"))
+           .andExpect(status().is3xxRedirection())
+           .andExpect(redirectedUrl("/game"));
+  }
+
 }
