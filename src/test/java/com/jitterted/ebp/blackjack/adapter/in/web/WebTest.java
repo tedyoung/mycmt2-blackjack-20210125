826557b986482e6e3c640d3ab0c53dc212fafd7f
Ted M. Young <tedyoung@gmail.com>
2021-01-27 20:14:52 -0800

    Passing test for POST to start the game.

--- src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/WebTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/WebTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest
@@ -20,4 +21,10 @@ public class WebTest {
            .andExpect(status().isOk());
   }
 
+  @Test
+  public void postToStartGameEndpointIs200Ok() throws Exception {
+    mockMvc.perform(post("/start-game"))
+           .andExpect(status().isOk());
+  }
+  
 }
